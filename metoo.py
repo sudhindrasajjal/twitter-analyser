b070681f0ad4b5da9ffe92c52ab6418594bafd98
sudhindra.s <sudhindra.s@media.net>
2017-10-18 02:27:05 +0530

    stream script

--- metoo.py
+++ metoo.py
@@ -3,12 +3,10 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-ACCESS_TOKEN = "84054662-kiW3PoweiZxxKydJf3g8J7PfZdSP8xKRajYJIbLTe"
-ACCESS_TOKEN_SECRET = "qf6Ruz5m1FCtZRmQfgu1pbjyIkNaeQaiMZX5CfyFbIc1T"
-API_KEY = "Q3PU20jlz1HGACxMhOx4OF4pR"
-API_SECRET = "JjYS4Qgu0fZMWGvHFNWfy5hxrQFSeT7LqLOqnaSfhrhC5nGMbU"
-
-# f = open("tweets.txt", "a+")
+ACCESS_TOKEN = "access-token"
+ACCESS_TOKEN_SECRET = "access-secret"
+API_KEY = "api-key"
+API_SECRET = "api-secret"
 
 
 class StdOutListener(StreamListener):
@@ -17,14 +15,11 @@ class StdOutListener(StreamListener):
     def on_data(self, data):
         """On data."""
         print data
-        # f.write(data)
         return True
 
     def on_error(self, status):
         """On error."""
         print status
-        # f.close()
-
 
 listener = StdOutListener()
 auth = OAuthHandler(API_KEY, API_SECRET)
